914231b008828353bc389ab6f22a1f5a4d635122
Samuel <samuelalves681@gmail>
2021-11-28 12:35:57 -0300

    menu principal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AnimationController, Platform, Animation } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  @ViewChild('menu', { read: ElementRef }) menu: ElementRef;
+  @ViewChild('score', { read: ElementRef }) score: ElementRef;
+  @ViewChild('game', { read: ElementRef }) game: ElementRef;
 
-  constructor() {}
+  hideMenuAnimation: Animation;
+  showGame: Animation;
+  showScore: Animation;
+  activeView = 'menu';
 
+  constructor(
+    private animationCtrl: AnimationController,
+    private plt: Platform
+  ) {}
+
+  ngAfterViewInit() {
+    const viewWidth = this.plt.width();
+
+    this.hideMenuAnimation = this.animationCtrl
+      .create('hide-menu')
+      .addElement(this.menu.nativeElement)
+      .duration(500)
+      .easing('ease-out')
+      .fromTo('opacity', 1, 0)
+      .fromTo('transform', 'translateX(0px)', `translateX(-${viewWidth}px)`);
+
+    this.showGame = this.animationCtrl
+      .create('game')
+      .addElement(this.game.nativeElement)
+      .duration(500)
+      .easing('ease-in')
+      .fromTo('opacity', 0, 1)
+      .fromTo('transform', `translateX(${viewWidth}px)`, 'translateX(0px)');
+
+    this.showScore = this.animationCtrl
+      .create('score')
+      .addElement(this.score.nativeElement)
+      .duration(500)
+      .easing('ease-in')
+      .fromTo('opacity', 0, 1)
+      .fromTo('transform', `translateX(${viewWidth}px)`, 'translateX(0px)');
+  }
+
+  openCategories() {
+    this.hideMenuAnimation.direction('alternate').play();
+    this.showGame.direction('alternate').play();
+    this.activeView = 'game';
+  }
+
+  openHighscore() {
+    this.hideMenuAnimation.direction('alternate').play();
+    this.showScore.direction('alternate').play();
+    this.activeView = 'score';
+  }
+
+  showMenu() {
+    this.hideMenuAnimation.direction('reverse').play();
+
+    if (this.activeView === 'game') {
+      this.showGame.direction('reverse').play();
+    } else {
+      this.showScore.direction('reverse').play();
+    }
+
+    this.activeView = 'menu';
+  }
 }
